8b8dde158b40fefebdfbb86b38dbcb8a282fe7d9
Leonardo <coeeu@hotmail.com>
2018-06-02 17:26:55 -0300

    Etapa 12. Populate the database

--- app/src/main/java/com/coesta/roomwordsample/db/WordRoomDatabase.java
+++ app/src/main/java/com/coesta/roomwordsample/db/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.coesta.roomwordsample.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import com.coesta.roomwordsample.db.dao.WordDao;
 import com.coesta.roomwordsample.model.Word;
@@ -15,19 +18,47 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
     private static WordRoomDatabase INSTANCE;
 
-    public static WordRoomDatabase getDatabase(final Context context){
-        if (INSTANCE == null){
-            synchronized (WordRoomDatabase.class){
-                if (INSTANCE == null){
+    public static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
                     //create database
                     INSTANCE = Room.databaseBuilder(
                             context.getApplicationContext(),
                             WordRoomDatabase.class,
                             "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase instance) {
+            mDao = instance.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            mDao.deleteAll();
+            Word word = new Word("Bahia");
+            mDao.insert(word);
+            word = new Word("Sergipe");
+            mDao.insert(word);
+            return null;
+        }
     }
 }
